c0325795d087573b7b69f304b48f3f3a75daf709
Sam Thompson <git@samt.us>
2019-09-09 13:23:40 -0700

    Add chai

--- server.test.js
+++ server.test.js
@@ -1,16 +1,22 @@
 'use strict';
 
+const chai = require('chai')
 const path = require('path');
+const fs = require('fs');
 const should = require('should');
 const sinon = require('sinon');
 const assert = require('assert');
 
+const expect = chai.expect;
+
+const keyFilename = path.join(__dirname, 'test/private.der');
+const certFilename = path.join(__dirname, 'test/public.der');
+const key = fs.readFileSync(keyFilename);
+const cert = fs.readFileSync(certFilename);
+const opts = { cert: certFilename, key: keyFilename };
+
 const dtls = require('./server');
 
-const opts = {
-	cert: path.join(__dirname, 'test/public.der'),
-	key: path.join(__dirname, 'test/private.der')
-};
 
 describe('server', function() {
 
