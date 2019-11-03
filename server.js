cae225b18b782ae8ef33630236b283dfcc4b77ae
Matthew McGowan <mat.mcgowan+git@gmail.com>
2019-11-03 03:52:37 +0100

    doc comments and clarifications and some pending research via todos.  No code changes.

--- server.js
+++ server.js
@@ -10,17 +10,43 @@ var mbed = require('bindings')('node_mbed_dtls.node');
 const APPLICATION_DATA_CONTENT_TYPE = 23;
 const IP_CHANGE_CONTENT_TYPE = 254;
 
+/**
+ * Emits:
+ *  "listening"     when the server is ready to accept incoming connections
+ *  "connection"    when a unverified client connects
+ *  "secureConnection" client, [session] when a client has connected and completed a handshake (session is undefined) or
+ *      when a session was resumed (session is non-null)
+ *  "clientError"   when an error occurs during the handshake
+ *  "error"         when an error occurs on the UDP socket. The socket is automatically closed after an error.
+ */
 class DtlsServer extends EventEmitter {
+	/**
+	 *
+	 * @param {object} options
+	 * @property sendClose - set to false when session resumption is required.
+	 *  todo - exactly why is it necessary to suppress the close event? Possibly so that when a server shuts down it can
+	 *  close each client connection without
+	 */
 	constructor(options) {
 		super();
 		this.options = options = Object.assign({
 			sendClose: true
 		}, options);
 
+		/**
+		 * Maps string keys to a DtlsClient instance
+		 * @type {object}
+		 * @see _createSocket
+		 */
 		this.sockets = {};
+
+		/**
+		 * The underlying networking socket that receives data for this server.
+		 */
 		this.dgramSocket = dgram.createSocket('udp4');
 		this._onMessage = this._onMessage.bind(this);
 		this.listening = false;
+		this._closing = false;
 
 		this.dgramSocket.on('message', this._onMessage);
 		this.dgramSocket.once('listening', () => {
@@ -48,10 +74,20 @@ class DtlsServer extends EventEmitter {
 		}
 	}
 
+	/**
+	 * Begin listening on the specified port and interface.
+	 * @param {Number} port
+	 * @param {String} hostname
+	 * @param {function) callback
+	 */
 	listen(port, hostname, callback) {
 		this.dgramSocket.bind(port, hostname, callback);
 	}
 
+	/**
+	 * Close this server socket and all clients.
+	 * @param callback  That is invoked once the socket is closed.
+	 */
 	close(callback) {
 		if (callback) {
 			this.once('close', callback);
@@ -71,6 +107,12 @@ class DtlsServer extends EventEmitter {
 		process.nextTick(() => callback(numConnections));
 	}
 
+	/**
+	 * Reinstates and resumes the session on a client socket based on the source address and session.
+	 * @param rinfo
+	 * @param session
+	 * @returns {boolean} false if there was already an existing client matching the same source
+	 */
 	resumeSocket(rinfo, session) {
 		const key = `${rinfo.address}:${rinfo.port}`;
 		let client = this.sockets[key];
@@ -80,6 +122,9 @@ class DtlsServer extends EventEmitter {
 
 		this.sockets[key] = client = this._createSocket(rinfo, key, true);
 		if (client.resumeSession(session)) {
+			// todo - in many ways it makes more sense to defer the secureConnection event until the first
+			// data is received and decoded from the peer
+			//
 			this.emit('secureConnection', client, session);
 			return true;
 		}
@@ -93,6 +138,7 @@ class DtlsServer extends EventEmitter {
 	}
 
 	_handleIpChange(msg, key, rinfo, deviceId) {
+		// this would be better named fetchSourceInfo and not exposed as an event but as a callback
 		const lookedUp = this.emit('lookupKey', deviceId, (err, oldRinfo) => {
 			if (!err && oldRinfo) {
 				// if the IP hasn't actually changed, handle normally
@@ -107,15 +153,15 @@ class DtlsServer extends EventEmitter {
 					const oldKey = `${oldRinfo.address}:${oldRinfo.port}`;
 					// if the message went through OK
 					if (received) {
-						this._debug(`message successfully received, changing ip address fromip=${oldKey}, toip=${key}, deviceID=${deviceId}`);
-						// change IP
-						client.remoteAddress = rinfo.address;
-						client.remotePort = rinfo.port;
+							this._debug(`message successfully received, changing ip address fromip=${oldKey}, toip=${key}, deviceID=${deviceId}`);
+							// change IP
+							client.remoteAddress = rinfo.address;
+							client.remotePort = rinfo.port;
 						// move in lookup table
 						this.sockets[key] = client;
-						delete this.sockets[oldKey];
-						// tell the world
-						client.emit('ipChanged', oldRinfo);
+							delete this.sockets[oldKey];
+							// tell the world
+							client.emit('ipChanged', oldRinfo);
 					} else {
 						//Do we need to jump out of lock state here too .. TBC (adding logging)?
 						this._debug(`message NOT successfully received NOT changing ip address fromip=${oldKey}, toip=${key}, deviceID=${deviceId}`);
@@ -128,7 +174,7 @@ class DtlsServer extends EventEmitter {
 				// mbedTLS to fail a socket read and initiate a handshake.
 				this._debug(`Device in 'move session' lock state attempting to force it to re-handshake deviceID=${deviceId}`);
 
-				//Always EMIT this event instead of calling _forceDeviceRehandshake internally this allows the DS to device wether to send the packet or not to the device
+				//Always EMIT this event instead of calling _forceDeviceRehandshake internally this allows the DS to device whether to send the packet or not to the device
 				this.emit('forceDeviceRehandshake', rinfo, deviceId);
 			}
 		});
@@ -153,8 +199,23 @@ class DtlsServer extends EventEmitter {
 		this.dgramSocket.send(malformedHelloRequest, rinfo.port, rinfo.address);
 	}
 
+	/**
+	 * Try resuming the session based on the source of the packet. The `resumeSession` event (really a callback)
+	 * is invoked to attempt to fetch the corresponding session for the source. If the session was successfully applied
+	 * to the mbedtls layer, the message is then attempted to be decoded. Only when this is successful is the "secureConnection"
+	 * event emitted.
+	 * @param client
+	 * @param msg
+	 * @param key
+	 * @param cb
+	 * @returns {boolean}
+	 * @private
+	 */
 	_attemptResume(client, msg, key, cb) {
 		const lcb = cb || (() => {});
+		// todo - this shouldn't be called resumeSession since it's more of a lookup and creates confusion with
+		// the other uses of resumeSession where a session is actually resumed.
+		// lookupSessionBySource would be more appropriate.
 		const called = this.emit('resumeSession', key, client, (err, session) => {
 			if (!err && session) {
 				const resumed = client.resumeSession(session);
@@ -182,6 +243,13 @@ class DtlsServer extends EventEmitter {
 		return called;
 	}
 
+	/**
+	 * Handle a packet from the socket.
+	 * @param msg       The message data
+	 * @param rinfo     The source address info
+	 * @param [cb]      Optional callback. If provided, events on the associated client are suppressed until the callback returns.
+	 * @private
+	 */
 	_onMessage(msg, rinfo, cb) {
 		const key = `${rinfo.address}:${rinfo.port}`;
 
@@ -197,19 +265,24 @@ class DtlsServer extends EventEmitter {
 
 			this._debug(`received ip change ip=${key}, deviceID=${deviceId}`);
 			if (this._handleIpChange(msg, key, rinfo, deviceId)) {
+				// _handleIpChange may recursively call _onMessage to handle this message, so we can drop out here.
 				return;
 			}
 		}
 
 		let client = this.sockets[key];
 		if (!client) {
+			// a device is sending data to this instance but we don't yet have a dtls socket for it
 			this.sockets[key] = client = this._createSocket(rinfo, key);
-
+			// the device is sending application data, meaning it considers the handshake done
+			// the session needs to be resumed before we can decode the application data.
 			if (msg.length > 0 && msg[0] === APPLICATION_DATA_CONTENT_TYPE) {
 				if (this._attemptResume(client, msg, key, cb)) {
 					return;
 				}
+				// else we are unable to resume. Attempt to just decode the message anyway by dropping through
 			}
+			// else it is a non-application message (alert, handshake etc..) attempt to decode
 		}
 
 		if (cb) {
@@ -224,6 +297,16 @@ class DtlsServer extends EventEmitter {
 		}
 	}
 
+	/**
+	 * Creates a new dtls socket representing a dtls connection with an endpoint
+	 * @param {object} rinfo     The address of the endpoint
+	 * @param {string} key       The unique identifier for the client
+	 * @param {boolean} selfRestored Set to `true` to indicate the socket was created by the server from a previously stored
+	 *  session. `false` when the socket is created as a result of a client connecting.
+	 *  Available as the `selfRestored` property on the returned object.
+	 * @returns {DtlsSocket}
+	 * @private
+	 */
 	_createSocket(rinfo, key, selfRestored) {
 		var client = new DtlsSocket(this, rinfo.address, rinfo.port);
 		client.sendClose = this.options.sendClose;
@@ -232,16 +315,26 @@ class DtlsServer extends EventEmitter {
 		return client;
 	}
 
+	/**
+	 * Attaches the necessary event listeners to the given DtlsSocket.
+	 * @param {DtlsSocket} client
+	 * @param key The key used to uniquely identify this client.
+	 * @private
+	 */
 	_attachToSocket(client, key) {
+		// todo - shouldn't the key be part of the socket rather than using the key bound to the outer scope?
+		// How is the correct key used after move session?
 		client.once('error', (code, err) => {
 			delete this.sockets[key];
 			if (!client.connected) {
 				this.emit('clientError', err, client);
 			}
+			// todo - don't we need to close the client
 		});
 		client.once('close', () => {
 			delete this.sockets[key];
 			client = null;
+			// once all keys have been deleted, close the server socket if it was being closed
 			if (this._closing && Object.keys(this.sockets).length === 0) {
 				this._closeSocket();
 			}
@@ -250,16 +343,21 @@ class DtlsServer extends EventEmitter {
 			// treat like a brand new connection
 			socket.reset();
 			this._attachToSocket(socket, key);
+			// todo - shouldn't we also post an event like sessionResumed or secureConnect?
 			this.sockets[key] = socket;
 		});
 
-		client.once('secureConnect', () => {
+		client.once('secureConnect', () => {    // after the handshake notify server listeners that this socket is available for use
 			this.emit('secureConnection', client);
 		});
 
-		this.emit('connection', client);
+		this.emit('connection', client);    // notify an unsecured connection is available
 	}
 
+	/**
+	 * Calls DtlsClient.end() on all client sockets.
+	 * @private
+	 */
 	_endSockets() {
 		if (this.dgramSocket) {
 			this.dgramSocket.removeListener('message', this._onMessage);
@@ -273,8 +371,15 @@ class DtlsServer extends EventEmitter {
 		});
 	}
 
+	/**
+	 * Handler for the closed event from the underlying network socket.
+	 * This method cleans up socket listeners, closes all client sockets and emits the "close" event and then
+	 * removes all listeners from this socket.
+	 * @private
+	 */
 	_socketClosed() {
 		this.listening = false;
+		// todo - why not call _endSockets() first and avoid the removeListener duplication?
 		if (this.dgramSocket) {
 			this.dgramSocket.removeListener('message', this._onMessage);
 		}
@@ -288,6 +393,7 @@ class DtlsServer extends EventEmitter {
 
 	_closeSocket() {
 		if (!this.listening) {
+			// simulate the socket closure when the socket has not started binding.
 			process.nextTick(() => {
 				this._socketClosed();
 			});
--- socket.js
+++ socket.js
@@ -7,8 +7,29 @@ var mbed = require('bindings')('node_mbed_dtls.node');
 const MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY = -0x7880;
 const MBEDTLS_ERR_SSL_CLIENT_RECONNECT = -0x6780;
 
+/**
+ * Implements a socket with a similar interface to other Duplex sockets. Note that the Readable interface
+ * of this implementation does not support paused mode, and consequently should be put in flowing mode by adding a
+ * "data" event listener.
+ *
+ *
+ * Emits:
+ *       "send", message.length: the length of the encrypted message sent
+ *       "secureConnect": secure connection has been established (handshake complete)
+ *       "error": only if a callback wasn't passed to write() (contrary to the node docs.) After the error event is emitted
+ *          the socket is torn down.
+ *       "closing": emitted when the connection is being torn down.
+ *       "close", hadError: the socket is finally torn down. First parameter indicates if the closure was due to an error
+ */
 class DtlsSocket extends stream.Duplex {
+	/**
+	 *
+	 * @param {DtlsServer} server   The server this client is bound to
+	 * @param {String} address      The remote ip address of the destination device
+	 * @param {Number} port         The remote port of the destination device
+	 */
 	constructor(server, address, port) {
+		// todo - when is the remote address and port updated when a Move Session is handled?
 		super({ allowHalfOpen: false });
 		this.server = server;
 		this.dgramSocket = server.dgramSocket;
@@ -16,6 +37,8 @@ class DtlsSocket extends stream.Duplex {
 		this.remotePort = port;
 		this._hadError = false;
 		this._sendClose = true;
+		// this._clientEnd = false;     // flag to indicate the socket is being ended by the local client
+		// this._ending = false;        // flag set when the socket is being torn down. Used to prevent re-entrant calls.
 		const key = `${address}:${port}`;
 
 		this.mbedSocket = new mbed.DtlsSocket(server.mbedServer, key,
@@ -45,6 +68,12 @@ class DtlsSocket extends stream.Duplex {
 		this._sendClose = value;
 	}
 
+	/**
+	 * Resume the mbedtls session state for this connection.
+	 * If the session is successfully resumed, the socket is considered connected (handshake complete) and resumed.
+	 * @param session
+	 * @returns {boolean}
+	 */
 	resumeSession(session) {
 		if (!session || !this.mbedSocket) {
 			return false;
@@ -61,20 +90,40 @@ class DtlsSocket extends stream.Duplex {
 		return success;
 	}
 
+	/**
+	 * Read data from the underlying stream. This presently does nothing since the data is delivered via the
+	 * {@link #receive} function. The stream only operates in flowing mode.
+	 * @private
+	 */
 	_read() {
 		// TODO implement way to stop/start reading?
 		// do nothing since chunk pushing is async
 	}
 
+	/**
+	 * Override the stream.Duplex `_write` method to write data to the stream.
+	 * This encodes the data via mbedSocket.send() method.
+	 * @param chunk
+	 * @param encoding
+	 * @param callback
+	 * @returns {*}
+	 * @private
+	 */
 	_write(chunk, encoding, callback) {
 		if (!this.mbedSocket) {
 			return callback(new Error('no mbed socket'));
 		}
 
-		this._sendCallback = callback;
+		this._sendCallback = callback;  // save the callback temporarily, it's picked up in _sendEncrypted
 		this.mbedSocket.send(chunk);
 	}
 
+	/**
+	 * Callback from the mbedtls library to send data to the network. This happens either intrinsically or in
+	 * response to sending application data via the `write` method.
+	 * @param {Buffer|Array} msg    The packet to send
+	 * @private
+	 */
 	_sendEncrypted(msg) {
 		// store the callback here because '_write' might be called
 		// again before the underlying socket finishes sending
@@ -84,6 +133,10 @@ class DtlsSocket extends stream.Duplex {
 			if (sendCb) {
 				sendCb(err);
 			}
+			// todo - what does this mean and why isit necessary?
+			//  _clientEnd is only set in end() which also calls _end().
+			// but it may not call _finishEnd(), but there appears to be a bug in that logic anyhow...
+			// maybe this was a quick fix for some strange behavior?
 			if (this._clientEnd) {
 				this._finishEnd();
 			}
@@ -125,6 +178,11 @@ class DtlsSocket extends stream.Duplex {
 			this._sendCallback(code);
 			this._sendCallback = null;
 		} else {
+			// todo - why not emit the error event even with a callback for general metrics gathering?
+			// The node docs say this (at https://nodejs.org/api/stream.html#stream_writable_write_chunk_encoding_callback)
+			// >> If an error occurs, the callback may or may not be called with the error as its first argument.
+			// >> To reliably detect write errors, add a listener for the 'error' event. If callback is called with
+			// >> an error, it will be called before the 'error' event is emitted.
 			this.emit('error', code, msg);
 		}
 		this._end();
@@ -152,6 +210,13 @@ class DtlsSocket extends stream.Duplex {
 		this.resumed = true;
 	}
 
+	/**
+	 * Accept an encrypted packet coming from the network and attempt to decode it.
+	 * Publishes the `receive` event with the incoming message length.
+	 * The decoded data (if any) is pushed to the Duplex stream's read buffer.
+	 * @param {Buffer|Array} msg The incoming message to decrypt
+	 * @returns {boolean}
+	 */
 	receive(msg) {
 		if (!this.mbedSocket) {
 			return false;
@@ -186,22 +251,35 @@ class DtlsSocket extends stream.Duplex {
 		this.connected = false;
 	}
 
+	/**
+	 * Tear down this connection.
+	 * @private
+	 */
 	_end() {
 		if (this._ending) {
 			return;
 		}
-		this._ending = true;
+		this._ending = true;    // prevent re-entrant calls
 
-		super.end();
-		this.push(null);
-		const noSend = !this._sendClose || this.mbedSocket.close();
+		super.end();            // signal that no more data will be written
+		this.push(null);        // signal that no more data can be read (EOF)
+		const noSend = !this._sendClose || this.mbedSocket.close(); // don't send the event if _sendClose is false or there was an error closing the socket.
 		this.emit('closing');
 		this.mbedSocket = null;
-		if (noSend || !this._clientEnd) {
+
+		// todo - this seems wrong - shouldn't the condition be !noSend ?
+		if (noSend || !this._clientEnd) {   // always finish if the connection was terminated by the remote client
 			this._finishEnd();
 		}
+		// todo - when _finishEnd() is not called, don't we end up with a bunch of listeners from _attachToSocket
+		// the spec says a Writable stream will always emit the close event even with the emitClose option
 	}
 
+	/**
+	 * Clean up this connection and emit the close event.
+	 *
+	 * @private
+	 */
 	_finishEnd() {
 		this.dgramSocket = null;
 		this.server = null;
